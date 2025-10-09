8dd34aa416c30d1eaa749bf0b25396936ddc8ed9
Capella87 <capella87@outlook.com>
2025-10-09 18:42:02 +0900

    feat(account): User registration

--- account/serializers.py
+++ account/serializers.py
@@ -1,11 +1,16 @@
+from typing import override
+
+from django.contrib.auth.models import AbstractUser
 from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
+from account.models import WishListUser
 
-class UserSignUpSerializer(serializers.Serializer):
+
+class UserSignUpSerializer(serializers.ModelSerializer):
     """
     id = models.AutoField(primary_key=True)
     email = models.EmailField(unique=True, blank=False, editable=True)
@@ -20,13 +25,23 @@ class UserSignUpSerializer(serializers.Serializer):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     """
+    password = serializers.CharField(max_length=200, required=True, allow_blank=False, write_only=True)
+    password2 = serializers.CharField(max_length=200, required=True, allow_blank=False, write_only=True)
 
-    email = serializers.EmailField(unique=True, required=True, allow_blank=False)
-    username = serializers.CharField(unique=True, required=True, allow_blank=False)
-    first_name = serializers.CharField(max_length=150, required=False, allow_blank=True)
-    last_name = serializers.CharField(max_length=150, required=False, allow_blank=True)
-    alias_name = serializers.CharField(max_length=150, required=False, allow_blank=True)
-    middle_name = serializers.CharField(max_length=150, required=False, allow_blank=True)
+    @override
+    def validate(self, attrs: dict) -> dict:
+        if not attrs.get('password') == attrs.get('password2'):
+            raise serializers.ValidationError('Passwords do not match.')
+        return attrs
 
+    @override
+    def create(self, validated_data: dict) -> AbstractUser:
+        validated_data.pop('password2')
+        user = self.Meta.model.objects.create_user(**validated_data)
 
+        return user
 
+    class Meta:
+        model = WishListUser
+        fields = ['email', 'username', 'first_name', 'last_name',
+                  'alias_name', 'middle_name', 'password', 'password2']
--- account/views.py
+++ account/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
+from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from account.models import WishListUser
+from account.serializers import UserSignUpSerializer
+
 
 # Create your views here.
+
+class UserAccountSignUpView(APIView):
+    serializer_class = UserSignUpSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request: Request) -> Response:
+        serializer = self.serializer_class(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+        return Response(data={'message': 'Successfully registered'},
+                        status=status.HTTP_201_CREATED)
