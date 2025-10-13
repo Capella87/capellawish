a58e482aa961ca490798e9c0e6f77277918d945a
Capella87 <capella87@outlook.com>
2025-10-13 11:33:23 +0900

    Add password validator configurations

--- capellawish/settings.py
+++ capellawish/settings.py
@@ -183,9 +183,15 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'OPTIONS': {
+            'max_similarity': 0.7
+        }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 8,
+        }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
