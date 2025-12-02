ac70da92967c9d868a7a547ba93f63146ec069a5
Capella87 <capella87@outlook.com>
2025-12-02 23:11:14 +0900

    chore(list): Make `image` read-only

--- list/serializers.py
+++ list/serializers.py
@@ -14,7 +14,7 @@ class ListSerializer(serializers.ModelSerializer):
     class Meta:
         model = ListModel
         fields = ['uuid', 'title', 'description', 'image', 'updated_at', 'item_count']
-        read_only_fields = ['uuid', 'updated_at', 'item_count']
+        read_only_fields = ['uuid', 'updated_at', 'item_count', 'image']
 
     def get_item_count(self, obj):
         return obj.items.count()
@@ -32,7 +32,7 @@ class ListDetailSerializer(serializers.ModelSerializer):
         model = ListModel
         fields = ['uuid', 'title', 'description', 'image', 'updated_at', 'item_count',
                   'created_at', 'allow_completion_by_other', 'allow_anonymous_completion', 'is_shared']
-        read_only_fields = ['uuid', 'created_at', 'updated_at', 'item_count']
+        read_only_fields = ['uuid', 'created_at', 'updated_at', 'item_count', 'image']
 
     def get_item_count(self, obj):
         return obj.items.count()
